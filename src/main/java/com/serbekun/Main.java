24b788ddf90516f6ed1a231cf246475b17039f7f
serbekun <sergey994049@gmail.com>
2026-08-20 14:22:26 +0900

    Update Main for init javalin server.

--- src/main/java/com/serbekun/Main.java
+++ src/main/java/com/serbekun/Main.java
@@ -1,9 +1,18 @@
 package com.serbekun;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.serbekun.bunkasai.BuildInfo;
+import com.serbekun.bunkasai.http.handles.V0Health;
+
+import io.javalin.Javalin;
+
+import com.serbekun.bunkasai.http.InitHttp;
+import com.serbekun.bunkasai.http.handles.HttpHandler;
+
 
 public final class Main {
 
@@ -13,5 +22,17 @@ public final class Main {
         
         log.info("Bunkasai!");
         log.info("Ver: " + BuildInfo.version());
+
+        /**
+         * 4. HTTP layer (handlers with DI)
+         */
+        List<HttpHandler> handlers = List.of(
+            new V0Health()
+        );
+
+        Javalin svr = Javalin.create();
+        InitHttp initHttp = new InitHttp(svr, 8080, handlers);
+        initHttp.initHttp();
+
     }
 }
